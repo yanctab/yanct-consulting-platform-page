dc8a50d77f246b91ff4caee9886dd3c84919e4c1
manszigher <mans.zigher@gmail.com>
2025-04-21 16:37:23 +0200

    Revert "Trying to fic the color"
    This reverts commit 8008775d1641f06cbcda3209ff296d5c2e8e0ccb.

--- src/components/TrianglePattern.tsx
+++ src/components/TrianglePattern.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TrianglePattern = ({
   opacity = 0.055,
   className = "",
-  color = "#F1F0FB",
+  color = "#F1F0FB", // default color
 }: {
   opacity?: number;
   className?: string;
@@ -19,12 +19,13 @@ const TrianglePattern = ({
     <svg
       className={`absolute inset-0 w-full h-full z-0 pointer-events-none ${className}`}
       viewBox={`0 0 ${width} ${height}`}
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       preserveAspectRatio="none"
       aria-hidden="true"
       style={{
         opacity,
-        mixBlendMode: "overlay", // optional, you can remove this if it interferes
+        mixBlendMode: "overlay",
       }}
     >
       {Array.from({ length: rows }).map((_, row) =>
@@ -35,7 +36,7 @@ const TrianglePattern = ({
             <polygon
               key={`${row}-${col}`}
               points={`${x},${y} ${x + size / 2},${y + size * 0.866} ${x - size / 2},${y + size * 0.866}`}
-              style={{ fill: color }}
+              fill={color}
             />
           );
         })
@@ -45,3 +46,4 @@ const TrianglePattern = ({
 };
 
 export default TrianglePattern;
+
