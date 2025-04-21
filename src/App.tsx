552b2b5283c7daa2bdd5b78aeb2231c1679883b0
manszigher <mans.zigher@gmail.com>
2025-04-21 22:23:16 +0200

    Revert "Fix: Ensure page renders correctly"
    This reverts commit 501889c43f24302340ba908b8c765ab4c651071e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { useEffect } from "react";
-
-// Create a wrapper component to handle URL parameters if needed
-const AppContent = () => {
-  const location = useLocation();
-  
-  useEffect(() => {
-    // Log when the app starts to help with debugging
-    console.log("App initialized with path:", location.pathname);
-    console.log("URL params:", location.search);
-  }, [location]);
-
-  return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-};
 
 const queryClient = new QueryClient();
 
@@ -35,7 +14,11 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <AppContent />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
