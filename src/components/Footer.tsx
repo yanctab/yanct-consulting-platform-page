3a62b7b7eb4c4c7b1ab574a70fce5e31b19e7468
manszigher <mans.zigher@gmail.com>
2025-04-21 16:49:31 +0200

    Footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
-
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-12">
       <div className="container mx-auto px-4">
         <div className="text-center">
           <div className="mb-4 flex justify-center">
-            <img src="/yanct.png" alt="Yanct Logo" className="h-12 w-auto" />
+            <img src="/yanct.png" alt="Yanct Logo" className="h-20 w-auto" />
           </div>
           <p className="text-gray-400">
             Transforming complex platforms into production-ready systems
@@ -17,3 +16,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
