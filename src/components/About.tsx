16e82e7af8d31ed1b3dc40f10d132c88ec74739f
manszigher <mans.zigher@gmail.com>
2025-04-21 17:55:21 +0200

    Vertical line

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -66,7 +66,7 @@ const About = () => {
                 className="hidden md:block absolute left-1/2 -translate-x-1/2 z-10"
                 style={{
                   top: '68px',
-                  height: 'calc(100% - 60px)',
+                  height: 'calc(100% - 20px)',
                   width: '0px',
                   borderLeft: '3px solid #fff',
                   pointerEvents: "none",
